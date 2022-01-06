dd9500e5e6c5a3b2a08879578894a18cbd7d2e76
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-01-06 19:58:36 +0500

    first commit3

--- src/Pages/Gochi_Shop/shop_index.jsx
+++ src/Pages/Gochi_Shop/shop_index.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import "./shop_index.css";
+import logo from "../../Assets/question-logo.png";
 function Shop() {
-    return (
-        <div className='shop'>
-            Shop
-        </div>
-    )
+  return (
+    <div className="shop">
+      <div className="shop_left">
+        <img src={logo} alt="" />
+      </div>
+      <div className="shop_right">
+        <h2>HATCH, TRAIN, EARN</h2>
+        <h1>MetaGochi Shop</h1>
+        <p>
+          Our Market Place will allow you to sell your trained MetaGochis to
+          others, or buy already trained MetaGochis to use in the MetaGochi
+          Games and earn $MGCHI rewards.
+        </p>
+        <p>
+          To be the first to find out when the MetaGotch store will launch join
+          our telegram group.
+        </p>
+        <button className="join-tele">
+          <i className="fab fa-telegram"></i>
+          Join Our Telegram
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Shop
+export default Shop;
--- src/Pages/Gochi_Verse/verse_index.jsx
+++ src/Pages/Gochi_Verse/verse_index.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import logo from "../../Assets/question-logo.png";
+import "./verse_index.css";
 function Verse() {
-    return (
-        <div className='verse'>
-            Verse
-        </div>
-    )
+  return (
+    <div className="verse">
+      <div className="verse_right">
+        <h1>The Metagochi Metaverse</h1>
+        <h2>Ready To Dive In?</h2>
+        <p>
+          The concept for the Metaverse may change as we progress through
+          development, but ultimately we envision a full Metagochi Metaverse
+          where you can train your Metagochi in order to have a stronger chance
+          of winning Metagochi Games and earning rewards in the form of unique
+          NFT eggs, $MGCHI tokens and items to be used in the game.
+        </p>
+        <p>
+          As we expand the development of the Metagochi Metaverse we will be
+          creating an augmented reality metaverse where you will be able to
+          train and interact with your Metagochi in the real world..
+        </p>
+        <button className="join-tele">
+          <i className="fab fa-telegram"></i>
+          Join Our Telegram
+        </button>
+      </div>
+      <div className="verse_left">
+        <img src={logo} alt="" />
+      </div>
+    </div>
+  );
 }
 
-export default Verse
+export default Verse;
