ec00684180ec7129182bf2343f30cdefefd70893
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-01-06 22:05:03 +0500

    v6

--- src/Pages/App/app_index.jsx
+++ src/Pages/App/app_index.jsx
@@ -10,18 +10,23 @@ function AppGochi() {
         <img src={phone} alt="..." />
         <img src={gif} alt="..." />
       </div>
-      <div className="app_right anim-right">
-        <h1>App Coming Soon</h1>
-        <p>
+      <div className="app_right">
+        <h1 className="anim-up">App Coming Soon</h1>
+        <p style={{ animationDelay: "0.3s" }} className="anim-right">
           We are currently in the process of creating a mobile marketplace app
           where non-crypto currency uses have the chance to invest into the game
           via rogue masks through our crypto/Fiat gateway
         </p>
-        <p>
+        <p style={{ animationDelay: "0.3s" }} className="anim-right">
           The release of this on Google play and App Store Will create a big
           push to mass adoption of the game driving prices up on in game nfts
         </p>
-        <img src={appStore} alt="..." className="app_img" />
+        <img
+          style={{ animationDelay: "0.7s" }}
+          src={appStore}
+          alt="..."
+          className="app_img fade-in"
+        />
       </div>
     </div>
   );
